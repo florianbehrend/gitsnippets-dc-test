1cb5ae433ab0c9dcdbe2c9ec631813f6d0810738
Florian Behrend <florian@livelyapps.com>
2025-03-17 17:38:28 +0100

    Setup Gitsnippets DC Test file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gitsnippets-dc-test
+
+This is a dummy file to test GitSnippets for Confluence on Confluence Data Center using Selenium.
---
+++ test.js
@@ -0,0 +1,10 @@
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+const num1 = 5;
+const num2 = 10;
+const result = add(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is ${result}`);
